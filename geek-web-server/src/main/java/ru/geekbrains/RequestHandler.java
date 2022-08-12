aa160d4babde42d5cbc09f4878d367290f87100e
adinchik <1chik.leoprog@gmail.com>
2022-08-12 10:26:07 +0600

    add code of teacher

--- geek-web-server/src/main/java/ru/geekbrains/RequestHandler.java
+++ geek-web-server/src/main/java/ru/geekbrains/RequestHandler.java
@@ -1,38 +1,49 @@
 package ru.geekbrains;
 
-import java.io.BufferedReader;
+import ru.geekbrains.service.FileService;
+import ru.geekbrains.service.SocketService;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.Deque;
 
 public class RequestHandler implements Runnable {
-    private final Socket socket;
-    private final String folder;
-    private WriteOutput writeOutput;
-    private RequestParsing requestParsing;
 
-    public RequestHandler(Socket socket, String folder) {
-        this.socket = socket;
-        this.folder = folder;
-        this.writeOutput = new WriteOutput(socket);
-        this.requestParsing = new RequestParsing();
+    private final SocketService socketService;
+
+    private final FileService fileService;
+
+    public RequestHandler(SocketService socketService, FileService fileService) {
+        this.socketService = socketService;
+        this.fileService = fileService;
     }
 
     @Override
     public void run() {
-        try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8))) {
-            while (!input.ready()) ;
-            String[] parts = requestParsing.getPartsOfFirstLineRequest(input.readLine());
-            while (input.ready()) {
-                System.out.println(input.readLine());
-            }
-            writeOutput.sendOutput(folder, parts[1]);
-            System.out.println("Client disconnected!");
+        Deque<String> rawRequest = socketService.readRequest();
+        String firstLine = rawRequest.pollFirst();
+        String[] parts = firstLine.split(" ");
 
-        } catch(IOException e) {
-            e.printStackTrace();
+        if (!fileService.exists(parts[1])) {
+            String rawResponse =
+                    "HTTP/1.1 404 NOT_FOUND\n" +
+                    "Content-Type: text/html; charset=utf-8\n" +
+                    "\n" +
+                    "<h1>Файл не найден!</h1>";
+            socketService.writeResponse(rawResponse);
+            return;
         }
 
+        String rawResponse = "HTTP/1.1 200 OK\n" +
+                "Content-Type: text/html; charset=utf-8\n" +
+                "\n" +
+                fileService.readFile(parts[1]);
+        socketService.writeResponse(rawResponse);
+
+        try {
+            socketService.close();
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
+        System.out.println("Client disconnected!");
     }
 }
